cf65a59bb0736b6b7b2b3130c0231eae7310db4f
Harry Jiang <harry10291993@hotmail.com>
2021-12-17 20:18:37 -0500

    Add E2E testing using cypress (Add, Edit, Cancel)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,7 @@ import Button from "components/Button";
 
 export default function Form(props) {
 
-  const { interviewers, onSave, onCancel } = props;
+  const { interviewers, onCancel } = props;
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,7 +9,7 @@ export default function DayListItem(props) {
   const dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected}, {'day-list__item--full': props.spots === 0 });
 
   return (
-    <li  className={dayClass} onClick={() => setDay(name)}>
+    <li  className={dayClass} onClick={() => setDay(name)} data-testid="day">
       <h2 className="text--regular">{name}</h2>
       {props.spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
       {props.spots === 1 && <h3 className="text--light">{spots} spot remaining</h3>}
